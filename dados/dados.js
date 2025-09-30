14307a10b13aa217f48463054d02cfd302e3528f
AlexandraLuiza-dot <alexandraluizamg@gmail.com>
2025-09-30 08:27:48 -0400

    amem?

---
+++ dados/dados.js
@@ -0,0 +1,139 @@
+const colecaoUf = [
+    {
+        id: 1,
+        uf: "AC",
+        nome: "Acre"
+    },
+    {
+        id: 2,
+        uf: "AL",
+        nome: "Alagoas"
+    },
+    {
+        id: 3,
+        uf: "AM",
+        nome: "Amazônia"
+    },
+    {
+        id: 4,
+        uf: "AM",
+        nome: "Amapá"
+    },
+    {
+        id: 5,
+        uf: "BA",
+        nome: "Bahia"
+    },
+    {
+        id: 6,
+        uf: "CE",
+        nome: "Ceará"
+    },
+    {
+        id: 7,
+        uf: "DF",
+        nome: "Distrito Federal"
+    },
+    {
+        id: 8,
+        uf: "ES",
+        nome: "Espiríto Santo"
+    },
+    {
+        id: 9,
+        uf: "GO",
+        nome: "Goiás"
+    },
+    {
+        id: 10,
+        uf: "MA",
+        nome: "Maranhão"
+    },
+    {
+        id: 11,
+        uf: "MG",
+        nome: "Minas Gerais"
+    },
+    {
+        id: 12,
+        uf: "MS",
+        nome: "Mato Grosso do Sul"
+    },
+    {
+        id: 13,
+        uf: "MT",
+        nome: "Mato Grosso"
+    },
+    {
+        id: 14,
+        uf: "PA",
+        nome: "Pará"
+    },
+    {
+        id: 15,
+        uf: "PB",
+        nome: "Paraíba"
+    },
+    {
+        id: 16,
+        uf: "PE",
+        nome: "Pernambuco"
+    },
+    {
+        id: 17,
+        uf: "PI",
+        nome: "Piaui"
+    },
+    {
+        id: 18,
+        uf: "PR",
+        nome: "Paraná"
+    },
+    {
+        id: 19,
+        uf: "RJ",
+        nome: "Rio de Janeiro"
+    },
+    {
+        id: 20,
+        uf: "RGN",
+        nome: "Rio Grande do Norte"
+    },
+    {
+        id: 21,
+        uf: "RO",
+        nome: "Rondonia"
+    },
+    {
+        id: 22,
+        uf: "RR",
+        nome: "Roraima"
+    },
+    {
+        id: 23,
+        uf: "RS",
+        nome: "Rio Grande do Sul"
+    },
+    {
+        id: 24,
+        uf: "ST",
+        nome: "Santa Catarina"
+    },
+    {
+        id: 25,
+        uf: "SE",
+        nome: "Sergipe"
+    },
+    {
+        id: 26,
+        uf: "SP",
+        nome: "Sao Paulo"
+    },
+    {
+        id: 27,
+        uf: "TO",
+        nome: "Tocantis"
+    }
+]
+
+export default colecaoUf;
---
+++ index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import { buscarUfs, buscarUfsPorId, buscarUfsPorNome } from './servicos/servico';
+
+const app = express();
+
+app.get('/ufs', (req,res) => {
+    const nomeUf = req.query.busca;
+    const resultado = nomeUf ? buscarUfsPorNome(nomeUf) : colecaoUf;
+    if (resultado.length > 0) {
+        res.json(resultado);
+    } else {
+        res.status(404).send({ "erro": "Nenhuma UF encontrada" })
+    }
+});
+
+app.get('/ufs/:iduf', (req,res) => {
+    const idUF = parseInt(req.params.idUF);
+    let mensagemErro = '';
+    let uf;
+
+    if (!(isNaN(idUF))) {
+        uf = colecaoUf.find(u => u.id === idUF);
+        if (uf) {
+            mensagemErro = "uf não encontrada";
+        }else {
+            mensagemErro = 'Requisição inválida';
+        }
+    };
+
+    if (uf) {
+        res.json(uf);
+    } else{
+        res;status(404).json({"erro": mensagemErro})
+    }     
+});
+
+app.listen(8080, () => {
+    let data = new Date();
+    console.log('Servidor iniciado na porta 8080 em: ' +data)
+});
